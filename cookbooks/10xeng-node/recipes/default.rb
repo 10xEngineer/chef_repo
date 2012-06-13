cd626fa59c0a320a9728a977ccf82154aa6cc3b0
Radim Marek <radim@laststation.net>
2012-06-13 15:24:40 +0200

    Fixed message

--- cookbooks/10xeng-node/recipes/default.rb
+++ cookbooks/10xeng-node/recipes/default.rb
@@ -68,12 +68,12 @@ end
 http_request "confirm node" do
   action :post
   url "#{node['microcloud']['endpoint']}/server/#{node['microcloud']['id']}/notify"
-  message Yajl::Encoder.encode {
+  message Yajl::Encoder.encode({
       :action => "confirm",
       :data => {
         :hostname => "TODO"
       }
-    }
-  headers({"Authorization" => "Basic #{Base64.encode64(node['10xeng-node']['token']})"})
+    })
+  headers({"Authorization" => "Basic #{Base64.encode64(node['10xeng-node']['token'])}"})
 end
 
