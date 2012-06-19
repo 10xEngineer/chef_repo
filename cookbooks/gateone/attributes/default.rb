122429c1d0d0a55f8f35dda638671142eb51934e
Radim Marek <radim@laststation.net>
2012-06-19 08:59:37 +0200

    Simple GateOne distribution packaging

--- cookbooks/gateone/attributes/default.rb
+++ cookbooks/gateone/attributes/default.rb
@@ -4,6 +4,8 @@ default["gateone"]["packages"] = ["python-tornado_2.2-1_all.deb", "gateone_1.0-2
 default["gateone"]["auth"] = nil
 default["gateone"]["users_dir"] = "/opt/gateone/users"
 
+default["gateone"]["commit"] = "684e0fc"
+
 # configuration parameters
 # TODO command
 # TODO syslog_host once the logging strategy is in place
