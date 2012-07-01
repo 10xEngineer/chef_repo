ef029c1680066d2eacbbb192013ff01b5ff830c1
Radim Marek <radim@laststation.net>
2012-07-01 13:30:41 +0200

    Upgraded Toolchain (VM UUID naming convention)

--- cookbooks/10xeng-node/attributes/default.rb
+++ cookbooks/10xeng-node/attributes/default.rb
@@ -5,7 +5,7 @@ default["microcloud"]["endpoint"] = nil
 default["10xeng-node"]["token"] = nil
 default["10xeng-node"]["id"] = nil
 
-default["10xeng-node"]["toolchain"]["version"] = "0.0.6"
+default["10xeng-node"]["toolchain"]["version"] = "0.0.7"
 default["10xeng-vm"]["packages"] = %W{ssh vim git curl lxc
 									  ruby1.9.3 build-essential}
 
