e1854f37d94fb7a6cb1767c20ff1d4c3ba49094b
Radim Marek <radim@laststation.net>
2012-08-16 10:36:35 +0200

    10xeng-vm: Moved instance specific data to VM descriptor

--- cookbooks/10xeng-node/attributes/default.rb
+++ cookbooks/10xeng-node/attributes/default.rb
@@ -5,7 +5,7 @@ default["microcloud"]["endpoint"] = nil
 default["10xeng-node"]["token"] = nil
 default["10xeng-node"]["id"] = nil
 
-default["10xeng-node"]["toolchain"]["version"] = "0.0.13"
+default["10xeng-node"]["toolchain"]["version"] = "0.0.14"
 default["10xeng-vm"]["packages"] = %W{ssh vim git curl lxc
 									  ruby1.9.3 build-essential}
 
