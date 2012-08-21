1c9bf120077d8067125f3c2dd1c1b63ee2d6f821
Radim Marek <radim@laststation.net>
2012-08-21 13:08:03 +0200

    Removed lxc & dialog from default LXC packages
    LXC forces bridge interface to be created by default (conflicts with the
    host network bridge)

--- cookbooks/lxc/attributes/default.rb
+++ cookbooks/lxc/attributes/default.rb
@@ -10,9 +10,9 @@ default["lxc-template"]["ubuntu"]["components"] = ["main","universe"]
 #
 # build fsrootcache from as many package as possible
 #
-default["lxc-template"]["ubuntu"]["packages"] = %W{dialog apt apt-utils iproute 
+default["lxc-template"]["ubuntu"]["packages"] = %W{apt apt-utils iproute 
 													inetutils-ping vim isc-dhcp-client 
 													isc-dhcp-common ssh lsb-release gnupg netbase 
 													ubuntu-keyring git curl 
-													lxc ruby1.9.3 build-essential vim}
+													ruby1.9.3 build-essential vim}
 
