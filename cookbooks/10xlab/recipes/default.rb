8b2915630b2f3bbbce9719e0ce221a1007d1403b
Radim Marek <radim@laststation.net>
2012-08-27 09:17:46 +0200

    Refactored MC API key retrieval into a ruby_block

--- cookbooks/10xlab/recipes/default.rb
+++ cookbooks/10xlab/recipes/default.rb
@@ -5,13 +5,24 @@ gem_package "microcloud" do
 end
 
 node["users"].each do |user|
+  ruby_block "get_key" do
+    block do
+      require 'microcloud'
+
+      microcloud = TenxLabs::Microcloud.new(node["microcloud"]["endpoint"])
+      key_data = microcloud.get(:key, user[:key])
+
+      if key_data
+        user[:ssh_public_key] = key_data["ssh_public_key"]
+      end    
+    end
+
+    action :create
+  end
+
   template "/tmp/#{user[:id]}-user_keys" do
     source "user_keys.erb"
-    require 'microcloud'
 
-    microcloud = TenxLabs::Microcloud.new(node["microcloud"]["endpoint"])
-    key = microcloud.get(:key, user[:key])
-
-    variables(:key => key["ssh_public_key"])
+    variables(:key => user[:ssh_public_key])
   end
 end
