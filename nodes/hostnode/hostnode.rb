1b99073a9a6a8fd1eb4dd91aaa98c8359f268c54
Radim Marek <radim@laststation.net>
2012-08-28 12:20:13 +0200

    Included hostnodes config/node details

---
+++ nodes/hostnode/hostnode.rb
@@ -0,0 +1,6 @@
+# part of binary distribution
+cookbook_path "/var/lib/10xeng/chef_repo/cookbooks"
+role_path "/var/lib/10xeng/chef_repo/roles"
+data_bag_path "/var/lib/10xeng/data_bags"
+file_cache_path "/var/chef"
+
