b4eaf1f503c89b27fc8f77fe8aefa9a38a0ac78f
Radim Marek <radim@laststation.net>
2012-08-31 13:22:48 +0200

    Hostnode toolchain uses JSON configuration file

--- cookbooks/10xeng-node/attributes/default.rb
+++ cookbooks/10xeng-node/attributes/default.rb
@@ -5,7 +5,7 @@ default["microcloud"]["endpoint"] = nil
 default["10xeng-node"]["token"] = nil
 default["10xeng-node"]["id"] = nil
 
-default["10xeng-node"]["toolchain"]["version"] = "0.0.14"
+default["10xeng-node"]["toolchain"]["version"] = "0.0.15"
 default["10xeng-vm"]["packages"] = %W{ssh vim git curl lxc
 									  ruby1.9.3 build-essential}
 default["10xeng-lxc"]["bootstrap"]["version"] = "0.4_all"
