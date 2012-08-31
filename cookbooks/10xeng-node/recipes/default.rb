88522d7b475a3b371c2dc7b13c839eade33b7442
Radim Marek <radim@laststation.net>
2012-08-31 11:20:10 +0200

    Switched to JSON config file format

--- cookbooks/10xeng-node/recipes/default.rb
+++ cookbooks/10xeng-node/recipes/default.rb
@@ -77,8 +77,8 @@ cookbook_file "/etc/init/lxc-net.conf" do
   notifies :restart, "service[lxc-net]"
 end
 
-template "/etc/10xlabs-hostnode.yaml" do
-  source "10xlabs-hostnode.yaml.erb"
+template "/etc/10xlabs-hostnode.json" do
+  source "10xlabs-hostnode.json.erb"
   mode "0644"
 end
 
