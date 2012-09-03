b5c57ad7482e87de353742cddc84aa10200e4025
Radim Marek <radim@laststation.net>
2012-09-03 14:55:43 +0200

    Updated default security credentials

--- cookbooks/10xeng-mc/recipes/default.rb
+++ cookbooks/10xeng-mc/recipes/default.rb
@@ -35,12 +35,13 @@ cookbook_file "/home/microcloud/wrap-ssh4git.sh" do
   mode 0755
 end
 
+# tenxeng:4haMWE4RoQGr
 cookbook_file "/home/microcloud/htpasswd" do
   source "htpasswd"
   owner "root"
   mode 0644
 
-  # FIXME restart nginx
+  notifies :restart, "service[nginx]"
 end
 
 # checkout latest microcloud code
