dbbfc18dcf0e33cee65db70c1a160d0e8a822afd
Radim Marek <radim@laststation.net>
2012-09-12 11:35:01 +0200

    Added sudo passwordless configuration for mchammer

--- roles/compile2.rb
+++ roles/compile2.rb
@@ -4,5 +4,14 @@ description "10xLabs compiler node (v2)"
 run_list(
   "role[base]",
   "recipe[10xlab-admins]",
+  "recipe[sudo]",
   "recipe[10xlab-compnode]"
 )
+
+override_attributes "authorization" => {
+  "sudo" => {
+    "groups" => ["admin"],
+    "users" => ["mchammer"],
+    "passwordless" => true
+  }
+}
