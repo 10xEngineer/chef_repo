e0b4d5594c3d4cb530e5e2ddf502824b67746721
Radim Marek <radim@laststation.net>
2012-09-25 22:47:47 +0200

    Compile service is now integrated within Microcloud API

--- cookbooks/10xlab-compnode/attributes/default.rb
+++ cookbooks/10xlab-compnode/attributes/default.rb
@@ -6,3 +6,4 @@ default["10xlab"]["compnode"]["home_location"] = "/mnt/sandbox"
 
 default["10xlab"]["compnode"]["version"] = "0.2_all"
 default["10xlab"]["compnode"]["compile_kits"] = ["10xlabs-definition_0.1_all"]
+
--- cookbooks/10xlab-githost/attributes/default.rb
+++ cookbooks/10xlab-githost/attributes/default.rb
@@ -1,5 +1,4 @@
 default["10xlab-githost"]["repo"] = "ssh://git@git.apac.10xlabs.net"
-default["10xlab-githost"]["compile"] = "ec2-46-137-196-240.ap-southeast-1.compute.amazonaws.com"
 default["10xlab-githost"]["version"] = "0.1_all"
 default["10xlab-githost"]["keys"] = [
 	"git.apac.10xlabs.net ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCS7GIZ4XQHZnlHZspC/wxdaP8qd4oySsdCGgmyEHzTai9r2eFATLF7DRKspcJew9+yqm0VwkpI52i5htsyfcig7OxQR8X7p7gT1+D+pyufFAWg/ZnW0RIK6y6rxZ0FcXZgIL80K0gQZMr1MMV3WMwdVrqvFsQjGT95dbYMW954iMucqA/huXvCjPXkRr56ZMTZOni8UBMBvEjE8zXacqmXuzJ+8LQC9XdldoE32EYDm5cAjstek8fZKpe1UP0Lmig5o3NuCH8wzSeXq40rDYqeEr2GKKo2hs/XoOea/b6EH0N4biIBtdKMPM3H0Gc2E6/G0mUJ0bUYbAnzHKv6oLDn"
