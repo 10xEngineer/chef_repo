45385023804060abfa3a0bf23e84cb994fcb74c7
Radim Marek <radim@laststation.net>
2012-09-26 12:06:27 +0200

    SSH wrapper adds sandbox identity; reused by compile service

--- cookbooks/10xlab-compnode/files/default/wrap-ssh4git.sh
+++ cookbooks/10xlab-compnode/files/default/wrap-ssh4git.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-/usr/bin/env ssh -A -o "StrictHostKeyChecking=no" -i "/tmp/mc_deploy" $1 $2
+identity_key=/mnt/sandbox/$(whoami)/.ssh/identity
+
+if [ -f $identity_key ]; then
+	/usr/bin/env ssh -i $identity_key -o "StrictHostKeyChecking=no" ${@}
+else
+	/usr/bin/env ssh -i $identity_key -o "StrictHostKeyChecking=no" ${@}
+fi
