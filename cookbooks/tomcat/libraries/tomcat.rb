282ac581d4b0651ad74d73a7ba6ea4af6f0b960d
Radim Marek <radim@laststation.net>
2012-10-01 14:00:46 +0200

    Better tomcat version expansion

--- cookbooks/tomcat/libraries/tomcat.rb
+++ cookbooks/tomcat/libraries/tomcat.rb
@@ -1,11 +1,12 @@
 module Tomcat
 	def version_parser(version) 
-		m = version.match /^(\d+\.)?(\d+\.)?(\*|\d+)?$/
+		m = version.match /^(\d)?.?(\d+)?.?(\d+)?/
+		versions = m.captures.map {|m| m ? m : "0"}
 
 		return {
-			:major => m[1].to_i,
-			:minor => m[2].to_i,
-			:build => m[3].to_i
+			:major => m[0].to_i,
+			:minor => m[1].to_i,
+			:build => m[2].to_i
 		}
 	end
 
