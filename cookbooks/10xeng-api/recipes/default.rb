faee749255ad74ef2bacfd2ffdab7de74c89bd38
Radim Marek <radim@laststation.net>
2012-11-04 11:20:58 +0100

    Platform API nginx templat

--- cookbooks/10xeng-api/recipes/default.rb
+++ cookbooks/10xeng-api/recipes/default.rb
@@ -7,6 +7,15 @@ service "api" do
   action [ :start ]
 end
 
+template "/etc/nginx/sites-available/labs_api.conf" do
+  owner "root"
+  group "root"
+
+  mode 0644
+
+  source "labs_api.conf.erb"
+end
+
 nginx_site "labs_api.conf" do
   enable true
 end
