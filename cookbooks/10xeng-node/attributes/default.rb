efb60724b9d17654f1ed2fb35e484e4b246f588d
Radim Marek <radim@laststation.net>
2012-11-05 20:32:22 +0100

    Update motd as part of machine provisioning

--- cookbooks/10xeng-node/attributes/default.rb
+++ cookbooks/10xeng-node/attributes/default.rb
@@ -5,6 +5,6 @@ default["microcloud"]["endpoint"] = nil
 default["10xeng-node"]["token"] = nil
 default["10xeng-node"]["id"] = nil
 
-default["10xeng-node"]["toolchain"]["version"] = "1.0.1"
+default["10xeng-node"]["toolchain"]["version"] = "1.0.2"
 default["10xeng-vm"]["packages"] = %W{ssh vim git curl lxc
 									  ruby1.9.3 build-essential}
