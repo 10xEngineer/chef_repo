f8b25440e324ace36ae961dead49ea6c38294b6f
Radim Marek <radim@laststation.net>
2012-11-23 09:04:59 +0100

    Updated toolchain to support persistent snapshots

--- cookbooks/10xeng-node/attributes/default.rb
+++ cookbooks/10xeng-node/attributes/default.rb
@@ -5,7 +5,7 @@ default["microcloud"]["endpoint"] = nil
 default["10xeng-node"]["token"] = nil
 default["10xeng-node"]["id"] = nil
 
-default["10xeng-node"]["toolchain"]["version"] = "1.2.3"
+default["10xeng-node"]["toolchain"]["version"] = "1.3.0"
 default["10xeng-vm"]["packages"] = %W{ssh vim git curl lxc
 									  ruby1.9.3 build-essential}
 
--- cookbooks/tarsnap/files/default/mongo_backup.sh
+++ cookbooks/tarsnap/files/default/mongo_backup.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+DATE=`date +%Y-%m-%d_%H-%M`
 
 /usr/bin/mongodump -o /root/dump >/dev/null
 
-/usr/local/bin/tarsnap --keyfile /root/.tarsnap/10xeng_gpi01-write --cachedir /root/.tarsnap/cache -c -f mongo_dump /root/dump 2>/dev/null
+/usr/local/bin/tarsnap --keyfile /root/.tarsnap/10xeng_gpi01-write --cachedir /root/.tarsnap/cache -c -f mongo_dump-${DATE} /root/dump 2>/dev/null
