5b3f04dd9fea8563f87115d43478745dbfb7beca
Radim Marek <radim@laststation.net>
2012-11-28 13:35:25 +0100

    Rest of the nodes using new deployment schema

--- nodes/shgate/solo.rb
+++ nodes/shgate/solo.rb
@@ -1,4 +1,4 @@
-file_cache_path "/var/chef"
-data_bag_path "/var/chef/data_bags"
-cookbook_path "/var/chef/cookbooks"
-role_path "/var/chef/roles"
+file_cache_path "/var/chef/repo"
+data_bag_path "/var/chef/repo/data_bags"
+cookbook_path "/var/chef/repo/cookbooks"
+role_path "/var/chef/repo/roles"
