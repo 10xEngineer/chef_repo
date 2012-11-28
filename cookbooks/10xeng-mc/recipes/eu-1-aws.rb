15590328eba9f20e740efac7f1b396fc9290af01
Radim Marek <radim@laststation.net>
2012-11-28 12:29:34 +0100

    Refactored deployment (using GIT)

--- cookbooks/10xeng-mc/recipes/eu-1-aws.rb
+++ cookbooks/10xeng-mc/recipes/eu-1-aws.rb
@@ -2,6 +2,26 @@
 
 include_recipe "nginx"
 
+cookbook_file "/etc/ssl/certs/eu-1-aws.crt" do
+	source "eu-1-aws.crt"
+	owner "root"
+	group "root"
+
+	mode 0644
+
+	action :create
+end
+
+cookbook_file "/etc/ssl/private/eu-1-aws.key" do
+	source "eu-1-aws.key"
+	owner "root"
+	group "root"
+
+	mode 0600
+
+	action :create
+end
+
 template "/etc/nginx/sites-available/api_eu-1-aws.conf" do
   owner "root"
   group "root"
--- shared/bootstrap.sh
+++ shared/bootstrap.sh
@@ -4,22 +4,31 @@ set -x
 
 # bootstrap
 ssh -A ubuntu@${TARGET} <<'ENDSSH'
-sudo apt-get clean
-sudo apt-get -y update
-sudo apt-get -y upgrade
+#sudo apt-get clean
+#sudo apt-get -y update
+#sudo apt-get -y upgrade
 #sudo apt-get -y install ruby1.9.3 build-essential vim linux-headers-$(uname -r)
 
 #sudo /usr/bin/gem install chef --no-ri --no-rdoc
 sudo /usr/bin/gem install ruby-shadow --no-ri --no-rdoc
 
-sudo rm -Rf /var/chef
-sudo mkdir -p /var/chef
+if [ ! -d /var/chef/repo ]; then
+	sudo rm -Rf /var/chef
+	sudo mkdir -p /var/chef
+
+	sudo chown ubuntu:ubuntu /var/chef
+
+	git clone git@github.com:10xEngineer/chef_repo.git /var/chef/repo
+else
+	cd /var/chef/repo
+	git pull
+fi
 ENDSSH
 
 # distribute
-gtar --exclude-vcs -cz -f /tmp/chef_repo.tar.gz .
-scp /tmp/chef_repo.tar.gz ubuntu@${TARGET}:/tmp/
+#gtar --exclude-vcs -cz -f /tmp/chef_repo.tar.gz .
+#scp /tmp/chef_repo.tar.gz ubuntu@${TARGET}:/tmp/
 # TODO replace 
 scp ../service/security/mc_deploy ubuntu@${TARGET}:/tmp/
-rm /tmp/chef_repo.tar.gz
+#rm /tmp/chef_repo.tar.gz
 
